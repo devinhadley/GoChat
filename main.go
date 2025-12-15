b1052d65860cdaa74f04b9e4aaaed19796effeb0
Devin Hadley <68879608+devinhadley@users.noreply.github.com>
2025-12-15 10:15:35 -0800

    logo & form styling

--- main.go
+++ main.go
@@ -11,15 +11,20 @@ func home(c *gin.Context) {
 	c.HTML(http.StatusOK, "home.html", gin.H{})
 }
 
+func login(c *gin.Context) {
+	c.HTML(http.StatusOK, "login.html", gin.H{})
+}
+
 func main() {
 	router := gin.Default()
 
-	router.Static("/static", "./css")
+	router.Static("/static", "./static")
 
 	router.LoadHTMLGlob("./templates/*")
 
 	// Routes...
 	router.GET("", home)
+	router.GET("/login", login)
 
 	err := router.Run()
 	if err != nil {
